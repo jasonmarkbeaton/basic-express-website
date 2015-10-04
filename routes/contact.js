db30e6e1c5e5f33fd25c60e231aae89fda96acdc
Jason Beaton <jason.beaton@gmail.com>
2015-10-04 17:40:16 -0400

    Added email submission feature

--- routes/contact.js
+++ routes/contact.js
@@ -7,5 +7,38 @@ router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
 
+// Send Email
+router.post('/send', function(req, res, next) {
+// create Reusable Transporter
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'hi@jasonmarkbeaton.com',
+        pass: 'something'
+    }
+});
+
+// Email Setup
+var mailOptions = {
+    from: 'John Doe <johndoe@gmail.com>',
+    to: 'hi@jasonmarkbeaton.com',
+    subject: 'Website Submission',
+    // Plain Text Version
+    text: 'You have a submission with the following details... Name: '+req.body.name +'Email: '+req.body.email +'Message: '+req.body.message,
+    // HTML Version
+    html: '<p>You got a website submission with the following details...</p><ul><li>Name: <b>'+req.body.name+'</b></li><li>Email: <b>'+req.body.email+'</b></li><li>Message: <b>'+req.body.message+'</b></li></ul>'
+};
+
+// Send
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+        res.redirect('/');
+    }else{
+        console.log('Message sent: ' + info.response);
+        res.redirect('/');
+    }
+});
+});
 
 module.exports = router;
